c2619f176929eaabd48c9e0aeafd6900f78d4d5d
Francois Ste-Marie <fstemarie@gmail.com>
2020-11-21 00:44:01 -0500

    Added outfile variable where I setup the filename step by step

--- img_to_array.py
+++ img_to_array.py
@@ -13,10 +13,8 @@ def main():
         t += "["
         for x in range(img.size[0]):  # for each pixels from that line
             if px[x, y] == 0:
-                # end="" means to not print a new line
                 t += "1" if inverse else "0"
             else:
-                # flush=false means to not write to the screen yet
                 t += "0" if inverse else "1"
             # Do not add a comma at the end if it's the last item
             t += "," if x < img.size[0] - 1 else ""
@@ -27,9 +25,13 @@ def main():
         t += "\n" # Add a new line
     t += "]\n"
 
-    out = open(os.path.join("output", f[0: -4] + ("_inverse" if inverse else "") + ".py"), "w")
-    out.write(t)
+    # Sets up the filename of the ouput file. Adds _inverse if the image is inversed
+    outfile = "{}{}.py".format(f[0: -4], "_inverse" if inverse else "")
+    outfile = os.path.join("output", outfile)
+    out = open(outfile, "w")
+    out.write(t) # Write the string to the output file
     out.close()
+    # Remove the commas and brackets so it looks better in the terminal
     t = t.replace(",", "").replace("[", "").replace("]", "")
     print(t)
 
