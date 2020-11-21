2fdd663a0b8213e075db3fbaba0ae1ccf2c0f382
Francois Ste-Marie <fstemarie@gmail.com>
2020-11-21 00:11:25 -0500

    New images, changed code a lot. added inverse

--- img_to_array.py
+++ img_to_array.py
@@ -1,36 +1,37 @@
 import os
 from PIL import Image
 
-f = "batman.png"
-
+inverse = True
+f = "ww.png"
 
 def main():
     img = Image.open(os.path.join("images", f))  # Open the file
-    # Converts image to greyscale (not binary)
-    # The algorythm used with mode "1" (binary) doesn't work well in this instance
-    img = img.convert("L")
     px = img.load()  # Put the pixels in an array
-    print(img.format, img.size, img.mode)
 
-    print("batman = [", flush=False)
+    t = "# {}\n{} = [\n".format(img.size, f[0: -4])
     for y in range(img.size[1]):  # for each lines of pixels
-        print("[", end="", flush=False)
+        t += "["
         for x in range(img.size[0]):  # for each pixels from that line
             if px[x, y] == 0:
                 # end="" means to not print a new line
-                print(0, end="", flush=False)
+                t += "1" if inverse else "0"
             else:
                 # flush=false means to not write to the screen yet
-                print(1, end="", flush=False)
-            if x < img.size[0] - 1:
-                # Do not add a comma at the end if it's the last item
-                print(",", end="", flush=False)
-        print("]", end="", flush=False)
+                t += "0" if inverse else "1"
+            # Do not add a comma at the end if it's the last item
+            t += "," if x < img.size[0] - 1 else ""
+        t += "]"
         if y < img.size[1] - 1:
             # Do not add a comma at the end if it's the last item
-            print(",", end="", flush=False)
-        print()  # Prints a new line
-    print("]")
+            t += ","
+        t += "\n" # Add a new line
+    t += "]\n"
+
+    out = open(os.path.join("output", f[0: -4] + ("_inverse" if inverse else "") + ".py"), "w")
+    out.write(t)
+    out.close()
+    t = t.replace(",", "").replace("[", "").replace("]", "")
+    print(t)
 
 
 if __name__ == "__main__":
